51926beb17e5cd713364ad7ffc705f7f15184afc
david-w-key <dwarrenkey27@gmail.com>
2017-10-14 14:03:38 -0400

    Added a review class

---
+++ app/src/main/java/bonetasticbooleans/gigme/Review.java
@@ -0,0 +1,30 @@
+package bonetasticbooleans.gigme;
+
+/**
+ * Created by dwarr on 10/14/2017.
+ */
+
+public class Review {
+    private int value;
+    private String comment;
+    private String name;
+
+    Review (int value, String comment, User user) {
+        this.value = value;
+        this.comment = comment;
+        this.name = User.getName();
+    }
+
+    public int getValue() {
+        return  value;
+    }
+
+    public String getComment() {
+        return comment;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+}
