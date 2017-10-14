761732979265187142babf41cf3433c9f3da5818
david-w-key <dwarrenkey27@gmail.com>
2017-10-14 11:33:39 -0400

    Fixing shit

---
+++ app/src/main/java/bonetasticbooleans/gigme/RegistrationActivity.java
@@ -0,0 +1,35 @@
+package bonetasticbooleans.gigme;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.CheckBox;
+
+import com.google.firebase.database.DatabaseReference;
+
+import bonetasticbooleans.gigme.R;
+
+/**
+ * Created by alan on 10/14/17.
+ * Represents the activity launched when a user is entering a registration screen
+ */
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        final String logTag = "RegistrationActivity";
+
+        final EditText usernameButton = (EditText) findViewById(R.id.usermameInput);
+        final EditText passwordButton = (EditText) findViewById(R.id.passwordInput);
+        final CheckBox checkbox = (CheckBox) findViewById(R.id.isMusicianCheckBox);
+
+        Button registerButton = (Button) findViewById(R.id.registerButton);
+
+
+    }
+}
---
+++ app/src/main/java/bonetasticbooleans/gigme/StartScreen.java
@@ -0,0 +1,28 @@
+package bonetasticbooleans.gigme;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class StartScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_screen);
+        System.out.print("Wtf");
+    }
+
+    public void onSignPressed(View v) {
+        Intent intent = new Intent(this, SignIn.class);
+        startActivity(intent);
+    }
+
+
+    public void onRegistrationPressed(View v) {
+        Intent intent = new Intent(StartScreen.this, RegistrationActivity.class);
+        startActivity(intent);
+    }
+}
