651a6404a4e9c1f8d1aad0627bd8bb75225f5508
david-w-key <dwarrenkey27@gmail.com>
2017-10-14 21:58:21 -0400

    Setting up FilloutPageClient activity

--- app/src/main/java/bonetasticbooleans/gigme/FilloutPageClient.java
+++ app/src/main/java/bonetasticbooleans/gigme/FilloutPageClient.java
@@ -2,12 +2,29 @@ package bonetasticbooleans.gigme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class FilloutPageClient extends AppCompatActivity {
+    private Spinner clientSpinner;
+    private EditText clientEmail;
+    private  EditText clientPhone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fillout_page_client);
+
+        clientSpinner = (Spinner) findViewById(R.id.cityClientSpinner);
+        clientEmail = (EditText) findViewById(R.id.clientEmail);
+        clientPhone = (EditText) findViewById(R.id.clientPhoneNumber);
+        
+        ArrayAdapter<City> adapter = new ArrayAdapter<City>(this, android.R.layout.simple_spinner_item, City.values());
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        clientSpinner.setAdapter(adapter);
+
+
+
     }
 }
