a2f789569d4b5e601db62780dd9f91a9fe43f3a3
Alan Li <alanli9898@gatech.edu>
2017-10-14 23:34:49 -0400

    bug fixes

--- app/src/main/java/bonetasticbooleans/gigme/FilloutActivity.java
+++ app/src/main/java/bonetasticbooleans/gigme/FilloutActivity.java
@@ -29,7 +29,7 @@ public class FilloutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fillout);
-        final Musician musician = savedInstanceState.getParcelable("musician");
+        final Musician musician = (Musician) savedInstanceState.getSerializable("musician");
 
         Spinner instrumentSpinner = (Spinner) findViewById(R.id.instrumentSpinner);
         Spinner genreSpinner = (Spinner) findViewById(R.id.genreSpinner);
