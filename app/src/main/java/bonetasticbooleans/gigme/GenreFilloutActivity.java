a52e90977538b3056c916c3c4620820ebd761acb
Alan Li <alanli9898@gatech.edu>
2017-10-15 02:06:57 -0400

    implemented genreFillout.xml

--- app/src/main/java/bonetasticbooleans/gigme/GenreFilloutActivity.java
+++ app/src/main/java/bonetasticbooleans/gigme/GenreFilloutActivity.java
@@ -1,8 +1,33 @@
 package bonetasticbooleans.gigme;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.CheckBox;
+
 /**
  * Created by alan on 10/15/17.
  */
 
-class GenreFilloutActivity {
+class GenreFilloutActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_genre_fillout);
+
+        Musician musician = (Musician) this.getIntent().getSerializableExtra("user");
+
+        CheckBox jazzCheckBox = (CheckBox) findViewById(R.id.jazzCheckBox);
+        CheckBox rockCheckbox = (CheckBox) findViewById(R.id.rockCheckBox);
+        CheckBox orchestraCheckBox = (CheckBox) findViewById(R.id.orchestraCheckBox);
+        CheckBox latinCheckBox = (CheckBox) findViewById(R.id.latinCheckBox);
+        CheckBox countryCheckBox = (CheckBox) findViewById(R.id.countryCheckBox);
+        CheckBox theatricalCheckBox = (CheckBox) findViewById(R.id.theatricalCheckBox);
+        CheckBox danceCheckBox = (CheckBox) findViewById(R.id.danceCheckBox);
+        CheckBox alternativeCheckBox = (CheckBox) findViewById(R.id.alternativeCheckBox);
+        CheckBox soloCheckBox = (CheckBox) findViewById(R.id.soloCheckBox);
+        CheckBox ambientCheckBox = (CheckBox) findViewById(R.id.ambientCheckBox);
+
+
+
+    }
 }
